4b7ff8df4a814f95f59e875472b8ce67650c0836
abinaya <69671704+abi-03-iba@users.noreply.github.com>
2021-06-09 15:09:00 +0530

    Update and rename cfg.py to config.py

--- cfg.py
+++
@@ -1,7 +0,0 @@
-get_database-->connect to the specific db called webscrap
-validate()-->validate an URL by checking whether a particular url returns a valid response or not
-insertedalready()-->function to check whether particular link already exists or not
-find_all('a')-->extract all anchor tags
-scrape() -->used for scrpping the links
-urlopen()-->for opening the url which we request
-ThreadPoolExecutor()-->thread pool will only have  concurrent threads that can process jobs
---
+++ config.py
@@ -0,0 +1,3 @@
+root="https://xxx.com" #paste the url of the website which you are going to crawl
+maxcount=5000
+
